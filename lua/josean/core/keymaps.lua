c2aa5c4695d14a8d7fc54d7c7d98bee20489637b
Soya <soya-kelaos@outlook.com>
2026-07-25 11:16:46 +0800

    fix: fixed blame display error

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -56,3 +56,5 @@ end, { desc = "Close buffer (skip NvimTree)" })
 keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format file via LSP" })
 
 keymap.set("n", "<leader>fo", "<cmd>Telescope lsp_document_symbols<CR>", { desc = "Document symbols" })
+
+keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<CR>", { desc = "Toggle git blame" })
--- lua/josean/plugins/git-blame.lua
+++ lua/josean/plugins/git-blame.lua
@@ -1,10 +1,11 @@
 return {
   "f-person/git-blame.nvim",
   event = "VeryLazy",
-  opts = {
-    date_format = "%Y-%m-%d",
-    message_template = "  %author%  (%date%) · %summary%",
-  },
+  config = function()
+    vim.g.gitblame_date_format = "%y-%m-%d %H:%M"
+    vim.g.gitblame_message_template = "  <author> <date> <sha>"
+    vim.g.gitblame_display_virtual_text = 1
+  end,
   keys = {
     { "<leader>gb", "<cmd>GitBlameToggle<CR>", desc = "Toggle git blame" },
   },
